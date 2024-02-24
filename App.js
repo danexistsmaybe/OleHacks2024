7eae827017d23a192358a37c08cc1d2c6b558442
Daniel Evans <danevans264@gmail.com>
2024-02-24 00:26:03 -0600

    big [ush

--- App.js
+++ App.js
@@ -1,10 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+
+import * as ImagePicker from 'expo-image-picker';
+import axios, {isCancel, AxiosError} from 'axios';
+
 
 export default function App() {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      //aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+
+    var bodyFormData = new FormData();
+    bodyFormData.append('image',image);
+
+    //POST
+    axios({
+      method: "post",
+      url: "localhost",
+      data: bodyFormData,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then(function (response) {
+        //handle success
+        console.log(response);
+      })
+      .catch(function (response) {
+        //handle error
+        console.log(response);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bingus bongiss</Text>
+      
+      {/* CAMERA STUFF */}
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +65,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: "lightblue",
+    color: "white",
   },
 });
