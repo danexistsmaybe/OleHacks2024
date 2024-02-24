3c61f77789640027778a78901b488bfb50ef5d3e
Joe Durben <joedurben@Gilgamesh.local>
2024-02-24 10:46:30 -0600

    fuck me

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, Image, Button, Image, TouchableOpacity } from 'react-native';
-
-const PlaceholderImage = require('MommyMisato.jpeg');
+import { Button, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 import { useState } from 'react';
-
 import * as ImagePicker from 'expo-image-picker';
 import axios, {isCancel, AxiosError} from 'axios';
+
+//const PlaceholderImage = require('MommyMisato.jpeg');
+
+//<Image source={PlaceholderImage} 
+    // style={{width: 400, heigh: 400}}
+     ///>
+//<Image style ={{
+      //width: 0,
+      //height: 0,
+    //}}
+      //source = {require(splash.png)}/>
 
 
 export default function App() {
@@ -15,18 +23,10 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     
-    <Image source={PlaceholderImage} 
-     //style={{width: 400, heigh: 400}}
-    />
-
-    <Image style ={{
-      width: 0,
-      height: 0,
-    }}
-      source = {require('MommyMisato.jpeg')}/>
 
     <Button
-      onPress = {onPressLearnMore}
+      onPress = {
+        alert('bitch ass mf')}
       title = "FUCKING HELP"
       color = "#841584"
       accessibiiltyLabel = "i dont know what this does"
@@ -44,7 +44,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e', //was #fff
+    backgroundColor: '#fff', //was #fff
     alignItems: 'center',
     justifyContent: 'center',
   },
