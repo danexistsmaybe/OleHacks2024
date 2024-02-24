9db3ee4368ba574a6430efe4711766bf35812622
Joe Durben <joedurben@Gilgamesh.local>
2024-02-24 10:12:07 -0600

    ahhhh

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
+
+const PlaceholderImage = require('MommyMisato.jpeg');
 
 export default function App() {
+  function onPressLearnMore() { 
+    alert('bitch ass mf');
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style ={{color: '#fff' }}>Open up App.js to start working on your app!</Text>
+      <Text>Bitches be btichen</Text>
+      <Text>8---------D</Text>
       <StatusBar style="auto" />
+    
+    <Image source={PlaceholderImage} 
+     //style={{width: 400, heigh: 400}}
+    />
+
+    <Image style ={{
+      width: 0,
+      height: 0,
+    }}
+      source = {require('MommyMisato.jpeg')}/>
+
+    <Button
+      onPress = {onPressLearnMore}
+      title = "FUCKING HELP"
+      color = "#841584"
+      accessibiiltyLabel = "i dont know what this does"
+    />
+
+      
+    
+      
+    
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e', //was #fff
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    felx: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+  },
+  button: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonLabel: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
