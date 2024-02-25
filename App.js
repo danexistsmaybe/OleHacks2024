1253e24a6a1d99c68264cabe08f32da4db7eefc8
Daniel Evans <danevans264@gmail.com>
2024-02-25 02:35:34 -0600

    final more or less

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 //import * as React from 'react';
-import { Button, View, Text, Image } from 'react-native';
+import { Button, View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { LinearGradient } from 'expo-linear-gradient';
 import { Camera, CameraType } from 'expo-camera';
 import { useState } from 'react';
 
@@ -12,22 +13,82 @@ import NewScreen from './NewScreen';
 const backendServerUrl = "http://10.42.25.0:8080/upload";
 //      <Image source={require('./assets/icon.png')} />
 
+// STYLES
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f4a460', //#483d8b
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    //elevation: 8,
+    //position: 'absolute',
+    backgroundColor: "FFFFFF", //#009688
+    borderRadius: 36,
+    paddingVertical: 25,
+    paddingHorizontal: 20,
+    elevation: 20,
+  },
+  shadow: {
+    shadowColor: 'black',
+    shadowOpacity: .5,
+    elevation: 8,
+    shadowRadius: 5,
+    shadowOffset : { width: 2, height: 13},
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  }
+});
+
 function HomeScreen({ navigation }) {
+  const AppButton = ({onPress, title}) => (
+    <TouchableOpacity style={styles.shadow} onPress = {onPress}> 
+      <LinearGradient
+        colors={["black", "grey", "red"]} //"#009688", "#004d40"
+        start= {{x:0, y:0}}
+        end={{x:2.5, y:.5}}
+        style={styles.button}
+
+      >
+        <Text style={styles.buttonText}>{title}</Text>
+        </LinearGradient>
+
+    </TouchableOpacity>
+  )
 
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Image source={require('./assets/icon.png')} style={{ width: 200, height: 200 }}/>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
+    <LinearGradient colors = {['white', 'grey']} start = {{x:0, y:0}} end = {{x:0, y:2}} style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Image source={require('./assets/icon.png')} style={{ width: 400, height: 400, top: -120 }}/>
+      <AppButton title="Press To Begin" onPress={() => navigation.navigate('Details')} />
+      <Text style={{fontStyle: 'italic', top: 100, marginLeft: 20, marginRight: 20, textAlign: 'center'}}>Disclaimer: this app uses artificial intelligence in its calculations. Some data may be misleading.</Text>
+    </LinearGradient>
   );
 }
 
 function DetailsScreen({ navigation }) {
+  const AppButton = ({onPress, title}) => (
+    <TouchableOpacity style={styles.shadow} onPress = {onPress}> 
+      <LinearGradient
+        colors={["black", "grey", "red"]} //"#009688", "#004d40"
+        start= {{x:0, y:0}}
+        end={{x:2.5, y:.5}}
+        style={styles.button}
+
+      >
+        <Text style={styles.buttonText}>{title}</Text>
+        </LinearGradient>
+
+    </TouchableOpacity>
+  )
+
+
   var image = null; 
 
   //const [image, setImage] = useState(null);
@@ -74,9 +135,7 @@ function DetailsScreen({ navigation }) {
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Select photo" onPress={() => {
-        pickImage();
-      }} />
+      <AppButton title="Take Picture" onPress={() => {pickImage();}} />
         {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }}
       />}
     </View>
--- NewScreen.js
+++ NewScreen.js
@@ -3,62 +3,33 @@ import { View, Text, ScrollView } from 'react-native';
 function NewScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'left', justifyContent: 'left', paddingLeft: 20 }}>
-    <ScrollView style={{flex:1}}><Text style={{fontWeight: 'bold',fontSize: 40,marginTop: 10}}>Flour</Text><Text>Calories: 200</Text>
-<Text>Fat: 0.0</Text>
+    <ScrollView style={{flex:1}}><Text style={{fontWeight: 'bold',fontSize: 40,marginTop: 10}}>Chocolate</Text><Text>Calories: 140</Text>
+<Text>Fat: 5.0</Text>
+<Text>Sat fat: 3.0</Text>
 <Text>Trans fat: 0.0</Text>
-<Text>Carbohydrates: 42.0</Text>
-<Text>Protein: 4.0</Text>
-<Text>Sodium: 0.0</Text>
-<Text>Iron: 2.16</Text>
-<Text>Serving size: 60.0GRM</Text>
-<Text style={{fontWeight: 'bold',fontSize: 40,marginTop: 10}}>Water</Text><Text>Calories: 101</Text>
-<Text>Fat: 0.0</Text>
-<Text>Carbohydrates: 26.0</Text>
-<Text>Protein: 0.0</Text>
-<Text>Sodium: 19.2</Text>
-<Text>Sugars: 26.0</Text>
-<Text>Serving size: 240.0ml</Text>
-<Text style={{fontWeight: 'bold',fontSize: 40,marginTop: 10}}>Salt</Text><Text>Calories: 0.0</Text>
-<Text>Fat: 0.0</Text>
-<Text>Carbohydrates: 0.0</Text>
-<Text>Protein: 0.0</Text>
-<Text>Sodium: 590</Text>
-<Text>Serving size: 1.5g</Text>
-<Text style={{fontWeight: 'bold',fontSize: 40,marginTop: 10}}>Sugar</Text><Text>Calories: 15.0</Text>
-<Text>Fat: 0.0</Text>
-<Text>Carbohydrates: 4.0</Text>
-<Text>Protein: 0.0</Text>
-<Text>Sodium: 0.0</Text>
-<Text>Sugars: 4.0</Text>
-<Text>Serving size: 4.0g</Text>
-<Text style={{fontWeight: 'bold',fontSize: 40,marginTop: 10}}>Tomato sauce</Text><Text>Calories: 20.1</Text>
-<Text>Fat: 0.0</Text>
-<Text>Sat fat: 0.0</Text>
-<Text>Trans fat: 0.0</Text>
-<Text>Carbohydrates: 4.0</Text>
+<Text>Carbohydrates: 25.0</Text>
 <Text>Protein: 1.0</Text>
 <Text>Cholesterol: 0.0</Text>
-<Text>Sodium: 260</Text>
-<Text>Fiber: 0.976</Text>
-<Text>Sugars: 2.0</Text>
+<Text>Sodium: 20.1</Text>
+<Text>Fiber: 0.986</Text>
+<Text>Sugars: 22.0</Text>
 <Text>Calcium: 0.0</Text>
-<Text>Iron: 0.72</Text>
-<Text>Potassium: 120</Text>
-<Text>Serving size: 61.0g</Text>
-<Text style={{fontWeight: 'bold',fontSize: 40,marginTop: 10}}>Cheese</Text><Text>Calories: 110</Text>
-<Text>Fat: 10.0</Text>
-<Text>Sat fat: 5.0</Text>
-<Text>Trans fat: 0.0</Text>
-<Text>Carbohydrates: 0.999</Text>
-<Text>Protein: 5.0</Text>
-<Text>Cholesterol: 30.0</Text>
-<Text>Sodium: 180</Text>
-<Text>Fiber: 0.0</Text>
-<Text>Sugars: 0.0</Text>
-<Text>Vitamind: 0.0</Text>
-<Text>Calcium: 151</Text>
 <Text>Iron: 0.0</Text>
-<Text>Potassium: 29.1</Text>
+<Text>Serving size: 34.0g</Text>
+<Text style={{fontWeight: 'bold',fontSize: 40,marginTop: 10}}>Nuts</Text><Text>Calories: 120</Text>
+<Text>Fat: 8.0</Text>
+<Text>Sat fat: 3.0</Text>
+<Text>Trans fat: 0.0</Text>
+<Text>Carbohydrates: 9.0</Text>
+<Text>Protein: 6.0</Text>
+<Text>Cholesterol: 0.0</Text>
+<Text>Sodium: 5.1</Text>
+<Text>Fiber: 2.01</Text>
+<Text>Sugars: 5.0</Text>
+<Text>Vitamind: 0.0</Text>
+<Text>Calcium: 9.9</Text>
+<Text>Iron: 5.04</Text>
+<Text>Potassium: 175</Text>
 <Text>Addedsugar: 0.0</Text>
 <Text>Serving size: 30.0GRM</Text>
 </ScrollView></View>
