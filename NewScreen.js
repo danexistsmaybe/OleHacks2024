e6ae097872f29cf29622b9e8831fef102bfd1c77
Daniel Evans <danevans264@gmail.com>
2024-02-25 21:03:02 -0600

    try again

--- NewScreen.js
+++ NewScreen.js
@@ -3,35 +3,32 @@ import { View, Text, ScrollView } from 'react-native';
 function NewScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'left', justifyContent: 'left', paddingLeft: 20 }}>
-    <ScrollView style={{flex:1}}><Text style={{fontWeight: 'bold',fontSize: 30,marginTop: 10}}>Chocolate</Text><Text>Calories: 140</Text>
-<Text>Fat: 5.0</Text>
-<Text>Sat fat: 3.0</Text>
+    <ScrollView style={{flex:1}}><Text style={{fontWeight: 'bold',fontSize: 30,marginTop: 10}}>Potatoes</Text><Text>Calories: 110</Text>
+<Text>Fat: 0.0</Text>
+<Text>Sat fat: 0.0</Text>
 <Text>Trans fat: 0.0</Text>
 <Text>Carbohydrates: 25.0</Text>
-<Text>Protein: 1.0</Text>
+<Text>Protein: 4.0</Text>
 <Text>Cholesterol: 0.0</Text>
-<Text>Sodium: 20.1</Text>
-<Text>Fiber: 0.986</Text>
-<Text>Sugars: 22.0</Text>
+<Text>Sodium: 0.0</Text>
+<Text>Fiber: 2.96</Text>
+<Text>Sugars: 1.01</Text>
 <Text>Calcium: 0.0</Text>
-<Text>Iron: 0.0</Text>
-<Text>Serving size: 34.0g</Text>
-<Text style={{fontWeight: 'bold',fontSize: 30,marginTop: 10}}>Nuts</Text><Text>Calories: 120</Text>
-<Text>Fat: 8.0</Text>
-<Text>Sat fat: 3.0</Text>
+<Text>Iron: 1.08</Text>
+<Text>Potassium: 650</Text>
+<Text>Serving size: 148.0g</Text>
+<Text style={{fontWeight: 'bold',fontSize: 30,marginTop: 10}}>Eggs</Text><Text>Calories: 90.1</Text>
+<Text>Fat: 6.0</Text>
+<Text>Sat fat: 2.0</Text>
 <Text>Trans fat: 0.0</Text>
-<Text>Carbohydrates: 9.0</Text>
-<Text>Protein: 6.0</Text>
-<Text>Cholesterol: 0.0</Text>
-<Text>Sodium: 5.1</Text>
-<Text>Fiber: 2.01</Text>
-<Text>Sugars: 5.0</Text>
-<Text>Vitamind: 0.0</Text>
-<Text>Calcium: 9.9</Text>
-<Text>Iron: 5.04</Text>
-<Text>Potassium: 175</Text>
-<Text>Addedsugar: 0.0</Text>
-<Text>Serving size: 30.0GRM</Text>
+<Text>Carbohydrates: 0.0</Text>
+<Text>Protein: 8.0</Text>
+<Text>Cholesterol: 235</Text>
+<Text>Sodium: 90.1</Text>
+<Text>Calcium: 39.7</Text>
+<Text>Iron: 1.08</Text>
+<Text>Potassium: 85.0</Text>
+<Text>Serving size: 63.0g</Text>
 </ScrollView></View>
 );
 }
